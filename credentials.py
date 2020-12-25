9e94b6f4f9360da9b7a6ab90c56afe4833524be3
sivanantha <70656917+sivanantha@users.noreply.github.com>
2020-12-25 10:30:54 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -6,10 +6,10 @@ cursor=connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS cred(username TEXT NOT NULL,password TEXT NOT NULL);")
 
 def register(username,pwd):
-	cursor.execute("SELECT * FROM cred WHERE username='{}'".format(username))
+	cursor.execute("SELECT * FROM cred WHERE username= ?",(username,))
 	user=cursor.fetchall()
 	if user == []:
-		cursor.execute("INSERT INTO cred(username,password) VALUES('{}','{}');".format(username,pwd))
+		cursor.execute("INSERT INTO cred(username,password) VALUES(?, ?)",(username, pwd,))
 		connection.commit()
 		connection.close()
 		return 1
@@ -18,19 +18,19 @@ def register(username,pwd):
 		return 0
 
 def forgot_password(username,pwd):
-	cursor.execute("SELECT * FROM cred WHERE username='{}'".format(username))
+	cursor.execute("SELECT * FROM cred WHERE username= ?",(username,))
 	user=cursor.fetchall()
 	if user == []:
 		connection.close()
 		return 0
 	else:
-		cursor.execute("UPDATE cred SET password='{}' WHERE username='{}'".format(pwd,username))
+		cursor.execute("UPDATE cred SET password= ? WHERE username= ?",(pwd, username,))
 		connection.commit()
 		connection.close()
 		return 1
 
 def authenticate(username,pwd):
-	cursor.execute("SELECT * FROM cred WHERE username='{}'".format(username))
+	cursor.execute("SELECT * FROM cred WHERE username= ?",(username,))
 	user=cursor.fetchall()
 	if user == []:
 		connection.close()
